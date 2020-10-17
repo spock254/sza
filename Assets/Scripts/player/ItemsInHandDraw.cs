1810eca6e7042474bfc66b5128d294dc1609b495
aleksandr <qwertyspock254@gmail.com>
2020-10-17 16:46:08 +0300

    items in hand draw

---
+++ Assets/Scripts/player/ItemsInHandDraw.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemsInHandDraw : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+    public Button left_hand_btn;
+    public Button right_hand_btn;
+
+    public GameObject leftHand;
+    public GameObject rightHand;
+
+    SpriteRenderer left;
+    SpriteRenderer right;
+
+    Vector2 up_down_left_position = new Vector2(-0.211f, -0.113f);
+    Vector2 up_down_right_position = new Vector2(0.211f, -0.113f);
+    Vector2 right_left_position = new Vector2(0, -0.113f);
+
+    private void Awake()
+    {
+        left = leftHand.GetComponent<SpriteRenderer>();
+        right = rightHand.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (playerMovement.input.y > 0)
+        {
+            rightHand.SetActive(true);
+            leftHand.SetActive(true);
+            
+            leftHand.transform.position = up_down_left_position + new Vector2(playerMovement.transform.position.x, playerMovement.transform.position.y);
+            rightHand.transform.position = up_down_right_position + new Vector2(playerMovement.transform.position.x, playerMovement.transform.position.y);
+
+            left.sortingOrder = right.sortingOrder = 4;
+        }
+        else if (playerMovement.input.y < 0)
+        {
+            rightHand.SetActive(true);
+            leftHand.SetActive(true);
+
+            leftHand.transform.position = up_down_left_position + new Vector2(playerMovement.transform.position.x, playerMovement.transform.position.y);
+            rightHand.transform.position = up_down_right_position + new Vector2(playerMovement.transform.position.x, playerMovement.transform.position.y);
+
+            left.sortingOrder = right.sortingOrder = 13;
+        }
+        else if (playerMovement.input.x > 0) 
+        {
+            rightHand.SetActive(false);
+            leftHand.SetActive(true);
+            leftHand.transform.position = right_left_position + new Vector2(playerMovement.transform.position.x, playerMovement.transform.position.y);
+            left.sortingOrder = right.sortingOrder = 13;
+        }
+        else if (playerMovement.input.x < 0)
+        {
+            leftHand.SetActive(false);
+            rightHand.SetActive(true);
+            rightHand.transform.position = right_left_position + new Vector2(playerMovement.transform.position.x, playerMovement.transform.position.y);
+            left.sortingOrder = right.sortingOrder = 13;
+        }
+
+        if (leftHand.activeInHierarchy) 
+        { 
+            left.sprite = left_hand_btn.GetComponent<Image>().sprite;   
+        }
+        if (rightHand.activeInHierarchy) 
+        { 
+            right.sprite = right_hand_btn.GetComponent<Image>().sprite;    
+        }
+    }
+}
--- Assets/Scripts/player/PlayerMovement.cs
+++ Assets/Scripts/player/PlayerMovement.cs
@@ -1,80 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 2f;
-
+    public Vector3 input = Vector3.zero;
+    //SpriteRenderer spriteRenderer;
     Rigidbody2D rigidBody;
+
+    public GameObject skinGo;
+    public GameObject skin_baseGo;
+    public GameObject hairGo;
+
+    SpriteRenderer skin;
+    SpriteRenderer skin_base;
+    SpriteRenderer hair;
+
+    [Header("skin")]
+    public Sprite up_skin;
+    public Sprite down_skin;
+    public Sprite left_skin;
+    public Sprite right_skin;
+
+    [Header("skin base")]
+    public Sprite up_skin_base;
+    public Sprite down_skin_base;
+    public Sprite left_skin_base;
+    public Sprite right_skin_base;
+
+    [Header("hair")]
+    public Sprite up_hair;
+    public Sprite down_hair;
+    public Sprite left_hair;
+    public Sprite right_hair;
 
     void Awake()
     {
+        //spriteRenderer = GetComponent<SpriteRenderer>();
         rigidBody = GetComponent<Rigidbody2D>();
+
+        skin = skinGo.GetComponent<SpriteRenderer>();
+        skin_base = skin_baseGo.GetComponent<SpriteRenderer>();
+        hair = hairGo.GetComponent<SpriteRenderer>();
+
     }
 
+    private void Start()
+    {
+        skin.sprite = down_skin;
+        hair.sprite = down_hair;
+        skin_base.sprite = down_skin_base;
+    }
 
     void FixedUpdate()
     {
-        Vector3 input = Vector3.zero;
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = input.normalized;
 
+        SetSprite(direction);
+
         Vector3 movement = direction * speed * Time.fixedDeltaTime;
 
         rigidBody.MovePosition(transform.position + movement);
     }
-    //public float moveSpeed = 4;
-    //private Vector2 playerDir;
-    //public static int x_player;
-    //public static int y_player;
-    //// Start is called before the first frame update
-    //void Start()
-    //{
 
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    Move();
-    //}
-
-    //private void Move()
-    //{
-    //    if (Input.GetAxis("Vertical") != 0) 
-    //    {
-    //        if (Input.GetAxis("Vertical") > 0)
-    //        {
-    //            y_player = 1;
-    //        }
-    //        else 
-    //        {
-    //            y_player = -1;
-    //        }
-
-    //        x_player = 0;
-
-    //        playerDir = new Vector2(0, Input.GetAxisRaw("Vertical"));
-    //    }
-    //    else if (Input.GetAxis("Horizontal") != 0) 
-    //    {
-    //        if (Input.GetAxis("Horizontal") > 0)
-    //        {
-    //            x_player = 1;
-    //        }
-    //        else
-    //        {
-    //            x_player = -1;
-    //        }
-
-    //        y_player = 0;
-
-    //        playerDir = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
-
-    //    }
-    //    transform.Translate(playerDir * moveSpeed * Time.deltaTime);
-    //}
+    void SetSprite(Vector2 dir) 
+    {
+        if (dir.x > 0)
+        {
+            skin.sprite = right_skin;
+            hair.sprite = right_hair;
+            skin_base.sprite = right_skin_base;
+        }
+        else if (dir.x < 0)
+        {
+            skin.sprite = left_skin;
+            hair.sprite = left_hair;
+            skin_base.sprite = left_skin_base;
+        }
+        else if (dir.y > 0)
+        {
+            skin.sprite = up_skin;
+            hair.sprite = up_hair;
+            skin_base.sprite = up_skin_base;
+        }
+        else if(dir.y < 0)
+        {
+            skin.sprite = down_skin;
+            hair.sprite = down_hair;
+            skin_base.sprite = down_skin_base;
+        }
+    }
 }
