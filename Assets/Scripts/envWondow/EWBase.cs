100289a92b54228f73fd6ca81f8cd3ae28d4a448
aleksandr <qwertyspock254@gmail.com>
2020-11-20 19:41:17 +0200

    ew form ch

---
+++ Assets/Scripts/envWondow/EWBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EWBase : MonoBehaviour
+{
+    protected GameObject window = null;
+
+    protected Controller controller = null;
+
+    protected float actioPlayerRadius = 0;
+    protected Vector2 ewindowPosition;
+    protected Transform player = null;
+
+    protected void BaseInit(GameObject window, GameObject envObj) 
+    {
+        controller = Global.Component.GetController();
+        player = controller.player;
+
+        this.window = window;
+        ewindowPosition = envObj.transform.position;
+        actioPlayerRadius = controller.GetActioPlayerRadius();
+    }
+
+    protected bool IsPlayerInEWindowRadius() 
+    {
+        return Vector2.Distance(player.position, ewindowPosition) < actioPlayerRadius;
+    }
+
+    public void Close()
+    {
+        Destroy(this.window);
+    }
+}
---
+++ Assets/Scripts/envWondow/EWFormCheckerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EWFormCheckerController : EWBase, IEWInit
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (IsPlayerInEWindowRadius() == false) 
+        {
+            Close();
+        }
+    }
+    public void Init(GameObject window, GameObject envObj)
+    {
+        BaseInit(window, envObj);
+    }
+}
