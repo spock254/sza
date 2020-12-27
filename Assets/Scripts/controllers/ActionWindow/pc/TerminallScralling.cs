8db2979dc6358f68e23ac9050fb986537383c259
aleksandr <qwertyspock254@gmail.com>
2020-12-27 19:34:59 +0200

    bo4 readme32

---
+++ Assets/Scripts/controllers/ActionWindow/pc/TerminallScralling.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminallScralling : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform commandLineContainerRect = null;
+    [SerializeField]
+    float yPos = 2.6f;
+    public void ValueChange(Vector2 pos) 
+    {
+        Debug.Log(commandLineContainerRect.position);
+        if (pos.y < 0.5f) 
+        {
+
+            commandLineContainerRect.position = new Vector3(commandLineContainerRect.position.x,
+                                                            yPos, 
+                                                            commandLineContainerRect.position.z);
+
+        }
+    }
+}
