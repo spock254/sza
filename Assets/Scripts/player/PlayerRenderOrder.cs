6f91f3eb3248955b20f037ca13beecee6e315f20
aleksandr <qwertyspock254@gmail.com>
2021-01-02 09:14:34 +0200

    player order render

---
+++ Assets/Scripts/player/PlayerRenderOrder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRenderOrder : MonoBehaviour
+{
+    const int ORDER_IN_LAYER_VALUE = 10;
+    
+    [SerializeField]
+    SpriteRenderer baseLayer = null;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "NPC") 
+        {
+            baseLayer.sortingOrder += ORDER_IN_LAYER_VALUE;        
+        }    
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "NPC")
+        {
+            baseLayer.sortingOrder -= ORDER_IN_LAYER_VALUE;
+        }
+    }
+}
