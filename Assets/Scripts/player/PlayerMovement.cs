d5297257aa4f58f6481d0ea8f991557e9a5f0382
aleksandr <qwertyspock254@gmail.com>
2021-01-16 19:05:00 +0200

    urp off

--- Assets/Scripts/player/PlayerMovement.cs
+++ Assets/Scripts/player/PlayerMovement.cs
@@ -33,50 +33,58 @@ public class PlayerMovement : MonoBehaviour
     {
 
     }
-    void FixedUpdate()
+
+    private void Update()
     {
-        if (!actionWindow.isOpen && !dialogWindow.isOpen)
-        {
-
-            input.x = Input.GetAxisRaw("Horizontal");
-            input.y = Input.GetAxisRaw("Vertical");
-
-            //if (input.x != 0 && input.y != 0)
-            //{
-            //    //twoButtonsPressed = true;
-            //    current2BtnsPress = 0;
-            //    prevDiagonPos = new Vector2(input.x, input.y);
-            //}
-            //else 
-            //{
-            //    current2BtnsPress += Time.deltaTime;
-            //    //twoButtonsPressed = false;
-            //}
-
-            isMoving = (input != Vector3.zero);
-
-            if (isMoving == true)
-            {
-                turn = input;
-            }
-            //else 
-            //{
-            //    //if (DIAGONAL_TIMEOUT <= current2BtnsPress)
-            //    //{
-            //    //    turn = prevDiagonPos;
-            //    //}
-            //    //else 
-            //    //{
-            //    //    prevDiagonPos = Vector2.zero;
-            //    //}
-            //}
-
-            Vector3 direction = input.normalized;
-            Vector3 movement = direction * speed * Time.fixedDeltaTime;
-            rigidBody.MovePosition(transform.position + movement);
-        
-        }
+        var input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        Vector3 velocity = input.normalized * speed;
+        transform.position += velocity * Time.deltaTime;
     }
+
+    //void FixedUpdate()
+    //{
+    //    if (!actionWindow.isOpen && !dialogWindow.isOpen)
+    //    {
+
+    //        input.x = Input.GetAxisRaw("Horizontal");
+    //        input.y = Input.GetAxisRaw("Vertical");
+
+    //        //if (input.x != 0 && input.y != 0)
+    //        //{
+    //        //    //twoButtonsPressed = true;
+    //        //    current2BtnsPress = 0;
+    //        //    prevDiagonPos = new Vector2(input.x, input.y);
+    //        //}
+    //        //else 
+    //        //{
+    //        //    current2BtnsPress += Time.deltaTime;
+    //        //    //twoButtonsPressed = false;
+    //        //}
+
+    //        isMoving = (input != Vector3.zero);
+
+    //        if (isMoving == true)
+    //        {
+    //            turn = input;
+    //        }
+    //        //else 
+    //        //{
+    //        //    //if (DIAGONAL_TIMEOUT <= current2BtnsPress)
+    //        //    //{
+    //        //    //    turn = prevDiagonPos;
+    //        //    //}
+    //        //    //else 
+    //        //    //{
+    //        //    //    prevDiagonPos = Vector2.zero;
+    //        //    //}
+    //        //}
+
+    //        Vector3 direction = input.normalized;
+    //        Vector3 movement = direction * speed * Time.fixedDeltaTime;
+    //        rigidBody.MovePosition(transform.position + movement);
+
+    //    }
+    //}
 
     public Vector3 GetTurnSide() 
     {
