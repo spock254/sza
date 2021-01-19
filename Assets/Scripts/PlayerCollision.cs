3a65a0079e86420038f01f18fbb191a06f8fc407
aleksandr <qwertyspock254@gmail.com>
2021-01-19 18:37:46 +0200

    new pc tiles 6

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public enum ColliderObject { GameObject, Wall }
+    
+    [SerializeField]
+    ColliderObject colliderObj = ColliderObject.Wall;
     PlayerMovement playerMovement;
 
     private void Start() 
@@ -16,22 +20,33 @@ public class PlayerCollision : MonoBehaviour
     {
         if (other.gameObject.tag == "player")
         {
-            Vector2 contact = Vector2.zero;
-
-            foreach (var contactPoints in other.contacts)
+            if (colliderObj == ColliderObject.Wall)
             {
-                if (contactPoints.normal.x == 1 || contactPoints.normal.x == -1)
-                {
-                    contact.x = contactPoints.normal.x;
-                }
+                Vector2 contact = Vector2.zero;
 
-                if (contactPoints.normal.y == 1 || contactPoints.normal.y == -1)
+                foreach (var contactPoints in other.contacts)
                 {
-                    contact.y = contactPoints.normal.y;
+                    if (contactPoints.normal.x == 1 || contactPoints.normal.x == -1)
+                    {
+                        contact.x = contactPoints.normal.x;
+                    }
+
+                    if (contactPoints.normal.y == 1 || contactPoints.normal.y == -1)
+                    {
+                        contact.y = contactPoints.normal.y;
+                    }
                 }
+                
+                playerMovement.SetDiractionAccess(contact);
             }
-            
-            playerMovement.SetDiractionAccess(contact);
+            else if (colliderObj == ColliderObject.GameObject)
+            {
+                //foreach (var contactPoints in other.contacts)
+                //{
+                //    Debug.Log(contactPoints.normal);
+                //}
+                playerMovement.SetDiractionAccess(other.contacts[0].normal);
+            }
         }   
     }
 
@@ -39,31 +54,49 @@ public class PlayerCollision : MonoBehaviour
     {
         if (other.gameObject.tag == "player")
         {
-            Vector2 contact = playerMovement.GetDiractionAccess();
-
-            if (other.contacts.Length == 1)
+            if (colliderObj == ColliderObject.Wall)
             {
-                if (contact != other.contacts[0].normal)
-                {
-                    playerMovement.SetDiractionAccess(other.contacts[0].normal);
-                }
-            }
-            else
-            {
-                Vector2 firstAx = other.contacts[0].normal;
-                Vector2 lastAx = other.contacts[other.contacts.Length - 1].normal;
+                Vector2 contact = playerMovement.GetDiractionAccess();
 
-                if ((((firstAx.x == 1 || firstAx .x == -1) && firstAx.y == 0) || ((firstAx.y == 1 || firstAx .y == -1) && firstAx.x == 0)) 
-                && (((lastAx.x == 1 || lastAx .x == -1) && lastAx.y == 0) || ((lastAx.y == 1 || lastAx .y == -1) && lastAx.x == 0)))
+                if (other.contacts.Length == 1)
                 {
-                    Vector2 axisSum = firstAx + lastAx;
-
-                    if (contact != axisSum)
+                    if (contact != other.contacts[0].normal)
                     {
-                        playerMovement.SetDiractionAccess(axisSum);
+                        playerMovement.SetDiractionAccess(other.contacts[0].normal);
                     }
                 }
+                else
+                {
+                    Vector2 firstAx = other.contacts[0].normal;
+                    Vector2 lastAx = other.contacts[other.contacts.Length - 1].normal;
 
+                    if ((((firstAx.x == 1 || firstAx .x == -1) && firstAx.y == 0) || ((firstAx.y == 1 || firstAx .y == -1) && firstAx.x == 0)) 
+                    && (((lastAx.x == 1 || lastAx .x == -1) && lastAx.y == 0) || ((lastAx.y == 1 || lastAx .y == -1) && lastAx.x == 0)))
+                    {
+                        Vector2 axisSum = firstAx + lastAx;
+
+                        if (contact != axisSum)
+                        {
+                            playerMovement.SetDiractionAccess(axisSum);
+                        }
+                    }
+
+                }
+            }
+            else if (colliderObj == ColliderObject.GameObject)
+            {
+                
+                Vector2 contact = new Vector2(Mathf.Round(other.contacts[0].normal.x), Mathf.Round(other.contacts[0].normal.y));
+                
+                if (playerMovement.GetDiractionAccess() != contact)
+                {
+                    playerMovement.SetDiractionAccess(contact);
+                }
+                //Debug.Log("___________________");
+                //foreach (var contactPoints in other.contacts)
+                //{
+                //    Debug.Log(contactPoints.normal);
+                //}
             }
         }   
     }
